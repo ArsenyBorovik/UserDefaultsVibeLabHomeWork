655f7d87c08fc777bc8c0af60151dc77fce8c088
Arseny <sinica2003@inbox.ru>
2023-12-26 20:57:22 -0500

    Initial Commit

---
+++ UserDefaultsVibeLabHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserDefaultsVibeLabHomeWork
+//
+//  Created by Арсений on 12/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
